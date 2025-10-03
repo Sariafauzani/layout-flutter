768fb99ca981ee90c04d195bcccc4fde7e7d083a
Sariafauzani <ajas2654@gmail.com>
2025-10-03 23:13:38 +0700

    add: Implementasi button row

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,7 @@ class MyApp extends StatelessWidget {
                 padding: EdgeInsets.only(bottom: 8),
                 child: Text(
                   'Wisata Pantai Modangan di Kabupaten Malang',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
               Text(
@@ -35,26 +33,56 @@ class MyApp extends StatelessWidget {
           ),
         ),
         /* soal 3 */
-        Icon(
-          Icons.star,
-          color: Colors.red,
-        ),
+        Icon(Icons.star, color: Colors.red),
         Text("41"),
       ],
     ),
   );
 
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Saria Fauzani - 2341760178',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter layout demo'),
+          title: const Text('Flutter layout demo'),
         ),
         body: Column(
           children: [
-            titleSection, // ganti Hello World dengan ini
+            titleSection,
+            buttonSection,
           ],
         ),
       ),
