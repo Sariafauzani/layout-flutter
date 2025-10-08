e8ffaf5d2f4ac934b46501c42ce008d035b970c6
Sariafauzani <ajas2654@gmail.com>
2025-10-08 23:05:38 +0700

    Tugas 2: add modalroute

--- belanja/lib/pages/item_page.dart
+++ belanja/lib/pages/item_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import '../models/item.dart';
 
 class ItemPage extends StatelessWidget {
   const ItemPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    throw UnimplementedError();
+    final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping List'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(itemArgs.name),
+            SizedBox(height: 10),
+            Text(itemArgs.price.toString()),
+          ],
+        ),
+      ),
+    );
   }
 }
